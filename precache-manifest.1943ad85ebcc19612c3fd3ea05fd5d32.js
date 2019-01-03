acb34d1419dd2122773df784b0e25aa5902c8b10
sunghyeon <sunghyeon86@gmail.com>
2019-01-03 17:52:19 +0900

    Updates

---
+++ precache-manifest.1943ad85ebcc19612c3fd3ea05fd5d32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e02ad2638ebf80a222f",
+    "url": "/My_App/static/js/runtime~main.5e02ad26.js"
+  },
+  {
+    "revision": "039e15311283c87b21f7",
+    "url": "/My_App/static/js/main.039e1531.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/My_App/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "039e15311283c87b21f7",
+    "url": "/My_App/static/css/main.b256c7f3.chunk.css"
+  },
+  {
+    "revision": "362cfe2a2782df6dde97d6a966b19d5b",
+    "url": "/My_App/index.html"
+  }
+];
