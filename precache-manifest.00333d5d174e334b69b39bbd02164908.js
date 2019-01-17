c66f4f308fb758e8a944f048d941f4fea3ec3c21
sunghyeon <sunghyeon86@gmail.com>
2019-01-17 21:25:58 +0900

    Updates

---
+++ precache-manifest.00333d5d174e334b69b39bbd02164908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "92724a349a0c2e32a7de",
+    "url": "/movie_app/static/js/main.92724a34.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "92724a349a0c2e32a7de",
+    "url": "/movie_app/static/css/main.b256c7f3.chunk.css"
+  },
+  {
+    "revision": "2cfb9a3521ff752a84ecd1ae7baa7c80",
+    "url": "/movie_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my_app/precache-manifest.8926abd39e87effbde4ac3630965071e.js"
+  "/movie_app/precache-manifest.00333d5d174e334b69b39bbd02164908.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my_app/index.html", {
+workbox.routing.registerNavigationRoute("/movie_app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
